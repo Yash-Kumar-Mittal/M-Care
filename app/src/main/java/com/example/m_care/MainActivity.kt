90d4f7fdbc37dcf101d199234e7afeee92ed7dad
Yash <yashmittal1704@gmail.com>
2022-11-14 19:44:35 +0530

    Sign in page created

---
+++ app/src/main/java/com/example/m_care/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.m_care
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            Intent(this,second_activity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+
+}
